e8ec0b18e7382166704f42e42aa085c90340d84c
panjialbir <panjialbir10@gmail.com>
2022-12-01 15:55:32 +0700

    1

--- Activity 6/Program.cs
+++ Activity 6/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+namespace StackUsingLinkedList_CSharp
+{
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
+    class Stacks
+    {
+        Node top;
+        public Stacks()
+        {
+            top = null;
+        }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+       
